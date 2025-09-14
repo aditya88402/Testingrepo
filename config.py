ef9d7d6e4074476a6b7b7c51932980051d6b4525
villainmusic777 <villainmusicgithub@gmail.com>
2025-09-14 11:32:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,24 +1,34 @@
 import re
 from os import getenv
-
+# ------------------------------------
+# ------------------------------------
 from dotenv import load_dotenv
 from pyrogram import filters
-
+# ------------------------------------
+# ------------------------------------
 load_dotenv()
-
-# Get this value from my.telegram.org/apps
+# ------------------------------------
+# -----------------------------------------------------
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
-
-# Get your token from @BotFather on Telegram.
+# ------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN")
+BOT_ID = getenv("BOT_ID")
+# -------------------------------------------------------
+OWNER_USERNAME = getenv("OWNER_USERNAME","iamakki001")
+# --------------------------------------------------------
+BOT_USERNAME = getenv("BOT_USERNAME" , "kuku_music_bot")
+# --------------------------------------------------------
+BOT_NAME = getenv("BOT_NAME" , "kuku music")
+# ---------------------------------------------------------
+ASSUSERNAME = getenv("ASSUSERNAME" , "kuku")
+# ---------------------------------------------------------
 
-# Get your mongo url from cloud.mongodb.com
-MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
-# Vars For API End Pont.
-YTPROXY_URL = getenv("YTPROXY_URL", 'https://tgapi.xbitcode.com') ## xBit Music Endpoint.
-YT_API_KEY = getenv("YT_API_KEY" , 'xbit_2IVTTLK1FWQABKH5JLHA0J' ) ## Your API key like: xbit_10000000xx0233 Get from  https://t.me/tgmusic_apibot
+#---------------------------------------------------------------
+#---------------------------------------------------------------
+MONGO_DB_URI = getenv("MONGO_DB_URI")
+API_KEY = getenv("API_KEY")
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 
@@ -41,18 +51,22 @@ OWNER_ID = int(getenv("OWNER_ID", 5909658683))
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # ----------------------------------------------------------------
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
+
+API_URL = getenv("API_URL", 'https://api.thequickearn.xyz') #youtube song url
+VIDEO_API_URL = getenv("VIDEO_API_URL", 'https://api.video.thequickearn.xyz')
+API_KEY = getenv("API_KEY", 'NxGBNexGenBots84d4d0') # youtube song api key
+#----------------------------------------------------------------
 # ----------------------------------------------------------------
 # ----------------------------------------------------------------
-# ----------------------------------------------------------------
-#SOURCE = getenv("SOURCE", "")
+SOURCE = getenv("SOURCE", "https://t.me/iamvillain77")
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/aditya88402/Heistsnetwork",
+    "https://github.com/pocof3gtrk/rpzl",
 )
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
-    "GIT_TOKEN", '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'
-)
+    "GIT_TOKEN", "")
+
 # --------------------------------------------------------------------
 # --------------------------------------------------------------------
 # --------------------------------------------------------------------
@@ -64,7 +78,8 @@ GIT_TOKEN = getenv(
 # -------------------------------------------------------------------------
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/iamvillain77")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/oldskoolgc")
-#-----------------------------------------------------
+CHAT = getenv("CHAT", "https://t.me/oldskoolgc")
+# ------------------------------------------------------------------------------
 # -------------------------------------------------------------------------------
 
 
@@ -77,7 +92,7 @@ SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/oldskoolgc")
 # --------------------------------------------------------------------------------
 # --------------------------------------------------------------------------------
 # --------------------------------------------------------------------------------
-AUTO_LEAVING_ASSISTANT = getenv("AUTO_LEAVING_ASSISTANT", "False")
+AUTO_LEAVING_ASSISTANT = getenv("AUTO_LEAVING_ASSISTANT", "True")
 AUTO_LEAVE_ASSISTANT_TIME = int(getenv("ASSISTANT_LEAVE_TIME", "9000"))
 SONG_DOWNLOAD_DURATION = int(getenv("SONG_DOWNLOAD_DURATION", "9999999"))
 SONG_DOWNLOAD_DURATION_LIMIT = int(getenv("SONG_DOWNLOAD_DURATION_LIMIT", "9999999"))
@@ -121,43 +136,45 @@ confirmer = {}
 # ------------------------------------
 # ------------------------------------
 # ------------------------------------
-# ------------------------------------
-
-# ------------------------------------------------------------------------
-# ------------------------------------------------------------------------
-# ------------------------------------------------------------------------
-# ------------------------------------------------------------------------
-# ------------------------------------------------------------------------
-# ------------------------------------------------------------------------
-# ------------------------------------------------------------------------
 
 STREAMI_PICS = [
-"https://i.ibb.co/whgkNq6n/start-img-1.jpg",
-"https://i.ibb.co/q32FdssH/start-img-2.jpg",
-
+"https://files.catbox.moe/4q7c4w.jpg",
+    "https://files.catbox.moe/90z6sq.jpg",
+    "https://files.catbox.moe/rdfi4z.jpg",
+    "https://files.catbox.moe/6f9rgp.jpg",
+    "https://files.catbox.moe/99wj12.jpg",
+    "https://files.catbox.moe/ezpnd2.jpg",
+    "https://files.catbox.moe/e7q55f.jpg",
+    "https://files.catbox.moe/qyfsi7.jpg",
+    "https://files.catbox.moe/kbke7s.jpg",
+    "https://files.catbox.moe/7icvpu.jpg",
+    "https://files.catbox.moe/4hd77z.jpg",
+    "https://files.catbox.moe/yn7wje.jpg",
+    "https://files.catbox.moe/kifsir.jpg",
+    "https://files.catbox.moe/zi21kc.jpg",
+    "https://files.catbox.moe/z0gh23.jpg",
+    "https://files.catbox.moe/f2s4ws.jpg",
+    "https://files.catbox.moe/26nzoq.jpg",
+    "https://files.catbox.moe/fu6jk3.jpg",
 ]
 
-START_IMG_URL = getenv(
-    "START_IMG_URL", "https://i.ibb.co/xPjc7tv/help-menu.jpg"
-)
 
 HELP_IMG_URL = getenv(
     "HELP_IMG_URL", "https://i.ibb.co/xPjc7tv/help-menu.jpg"
 )
 
-PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://i.ibb.co/VWnm6f3f/ping.jpg"
-)
-PLAYLIST_IMG_URL = "https://i.ibb.co/gL3ykkyh/play-music.jpg"
-STATS_IMG_URL = "https://i.ibb.co/pBqPtFYn/statistics.jpg"
-TELEGRAM_AUDIO_URL = "https://i.ibb.co/gL3ykkyh/play-music.jpg"
-TELEGRAM_VIDEO_URL = "https://i.ibb.co/gL3ykkyh/play-music.jpg"
-STREAM_IMG_URL = "https://i.ibb.co/0VKCS20y/stream.jpg"
-SOUNCLOUD_IMG_URL = "https://i.ibb.co/S4sPf3q8/soundcloud.jpg"
-YOUTUBE_IMG_URL = "https://i.ibb.co/xShkBVBK/youtube.jpg"
-SPOTIFY_ARTIST_IMG_URL = "https://i.ibb.co/XZfMS8Db/spotify.jpg"
-SPOTIFY_ALBUM_IMG_URL = "https://i.ibb.co/XZfMS8Db/spotify.jpg"
-SPOTIFY_PLAYLIST_IMG_URL = "https://i.ibb.co/XZfMS8Db/spotify.jpg"
+START_IMG_URL = getenv("START_IMG_URL", "https://files.catbox.moe/fu6jk3.jpg")
+PING_IMG_URL = getenv("PING_IMG_URL", "https://files.catbox.moe/26nzoq.jpg")
+PLAYLIST_IMG_URL = "https://files.catbox.moe/f2s4ws.jpg"
+STATS_IMG_URL = "https://files.catbox.moe/z0gh23.jpg"
+TELEGRAM_AUDIO_URL = "https://files.catbox.moe/2y5o3g.jpg"
+TELEGRAM_VIDEO_URL = "https://files.catbox.moe/2y5o3g.jpg"
+STREAM_IMG_URL = "https://telegra.ph/file/d30d11c4365c025c25e3e.jpg"
+SOUNCLOUD_IMG_URL = "https://telegra.ph/file/d30d11c4365c025c25e3e.jpg"
+YOUTUBE_IMG_URL = "https://files.catbox.moe/2y5o3g.jpg"
+SPOTIFY_ARTIST_IMG_URL = "https://files.catbox.moe/2y5o3g.jpg"
+SPOTIFY_ALBUM_IMG_URL = "https://files.catbox.moe/2y5o3g.jpg"
+SPOTIFY_PLAYLIST_IMG_URL = "https://telegra.ph/file/d30d11c4365c025c25e3e.jpg"
 
 # ------------------------------------------------------------------------
 # ------------------------------------------------------------------------
