ae68c40b71f731e4eef55a8a351e03b239ba0253
aditya88402 <aditya266015@gmail.com>
2025-09-17 00:15:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -52,8 +52,8 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # ----------------------------------------------------------------
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
-API_URL = getenv("API_URL", 'https://api.thequickearn.xyz') #youtube song url
-VIDEO_API_URL = getenv("VIDEO_API_URL", 'https://api.video.thequickearn.xyz')
+API_URL = getenv("API_URL", 'https://tgapi.xbitcode.com') #youtube song url
+VIDEO_API_URL = getenv("VIDEO_API_URL", 'https://tgapi.xbitcode.com')
 API_KEY = getenv("API_KEY", '') # youtube song api key
 #----------------------------------------------------------------
 # ----------------------------------------------------------------
